f82575d801f16abbeedb36753566c763e2b8a2f7
NAZIYA BEGUM <naziyamirza04@gmail.com>
2025-02-25 15:40:07 +0530

    3rd project completed

---
+++ Digital clock/script.js
@@ -0,0 +1,10 @@
+let clock= document.getElementById('clock');
+// let time= document.getElementByClass('clock');
+// let time= document.querySelector('#clock');
+
+//we want to display time after every second, therefore we need something that works in a given interval of time.
+//takes two params, one function and other one is time.
+setInterval(function(){
+    let date= new Date();
+    clock.innerHTML= date.toLocaleTimeString();
+}, 1000);
